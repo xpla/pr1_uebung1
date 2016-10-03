90195d7c14c9fbbf74fc4021fe556215107d752a
Christian Kovacic <chris.k@gmx.at>
2016-10-03 21:58:15 +0200

    Übung 3 eingearbeitet

---
+++ src/pr1_uebung1/Temperatur.java
@@ -0,0 +1,17 @@
+package pr1_uebung1;
+
+public class Temperatur {
+
+	public static double celsius2Fahrenheit(double celsius) {
+		double Fahrenheit = 9/5;
+		return Fahrenheit;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(celsius2Fahrenheit(32));
+		
+	}
+
+}
